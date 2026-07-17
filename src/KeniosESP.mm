bc648104f61bec022a559c162d76ae0f76b141b0
minhchien434850-hash <minhchien434850@gmail.com>
2026-07-17 17:54:06 +0700

    Refactor KeniosESP to KeniosFPS for performance display

--- src/KeniosESP.mm
+++ src/KeniosESP.mm
@@ -1,132 +1,158 @@
+// =============================================
+// KENIOS HAX - FPS & Performance Module
+// iOS 16.0-26.5
+// =============================================
+
 #import "KeniosCommon.h"
-#import "KeniosOffsets.h"
 #import "KeniosConfig.h"
 
-@interface KeniosESPOverlay : UIView
-@property (nonatomic, strong) NSMutableArray *players;
-@property (nonatomic, strong) NSMutableArray *bombs;
-@property (nonatomic, strong) KeniosESPConfig *config;
-@property (nonatomic, assign) KeniosMatrix4x4 viewMatrix;
-@property (nonatomic, assign) CGFloat sw, sh;
+@interface KeniosFPSOverlay : UILabel
+@property (nonatomic, assign) int currentFPS;
+@property (nonatomic, assign) int currentPing;
+@property (nonatomic, strong) UIColor *displayColor;
+@property (nonatomic, assign) int position;
 @end
 
-@implementation KeniosESPOverlay
+@implementation KeniosFPSOverlay
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
-    if (self) { self.backgroundColor = [UIColor clearColor]; self.userInteractionEnabled = NO; self.players = [NSMutableArray new]; self.bombs = [NSMutableArray new]; self.config = [KeniosConfig sharedInstance].esp; }
+    if (self) {
+        self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.6];
+        self.textColor = [UIColor greenColor];
+        self.font = [UIFont boldSystemFontOfSize:12];
+        self.textAlignment = NSTextAlignmentCenter;
+        self.layer.cornerRadius = 8;
+        self.clipsToBounds = YES;
+        self.numberOfLines = 2;
+        self.currentFPS = 60;
+        self.currentPing = 0;
+        self.displayColor = [UIColor greenColor];
+    }
     return self;
 }
 
-- (void)drawRect:(CGRect)rect {
-    if (!self.config.enabled) return;
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    if (!ctx) return;
+- (void)updateDisplay {
+    NSString *text = [NSString stringWithFormat:@"FPS: %d\n📡 %dms", self.currentFPS, self.currentPing];
+    self.text = text;
     
-    for (NSValue *v in self.players) {
-        KeniosPlayer *p = (KeniosPlayer *)[v pointerValue];
-        if (!p || !p->isAlive) continue;
-        [self renderPlayer:p ctx:ctx];
+    if (self.currentFPS >= 55) {
+        self.textColor = [UIColor greenColor];
+    } else if (self.currentFPS >= 30) {
+        self.textColor = [UIColor yellowColor];
+    } else {
+        self.textColor = [UIColor redColor];
     }
-    for (NSValue *v in self.bombs) {
-        KeniosBomb *b = (KeniosBomb *)[v pointerValue];
-        if (b->isActive) [self renderBomb:b ctx:ctx];
-    }
-    if (self.config.fovCircleEnabled) [self drawFOVCircle:ctx];
-    if (self.config.crosshairEnabled) [self drawCrosshair:ctx];
 }
 
-- (void)renderPlayer:(KeniosPlayer *)p ctx:(CGContextRef)ctx {
-    KeniosVector2 head = [self w2s:p->head], pelvis = [self w2s:p->pelvis];
-    if (head.x < 0 || pelvis.x < 0) return;
-    CGFloat bh = pelvis.y - head.y, bw = bh * 0.4f, bx = head.x - bw/2, by = head.y;
-    UIColor *color = p->isTeammate ? self.config.boxColorTeam : p->isBot ? self.config.boxColorBot : self.config.boxColorVisible;
-    
-    if (self.config.boxEnabled) { CGContextSetStrokeColorWithColor(ctx, color.CGColor); CGContextSetLineWidth(ctx, self.config.boxThickness); CGContextStrokeRect(ctx, CGRectMake(bx, by, bw, bh)); }
-    if (self.config.skeletonEnabled) { CGContextSetStrokeColorWithColor(ctx, self.config.skeletonColorVisible.CGColor); CGContextSetLineWidth(ctx, self.config.skeletonThickness);
-        KeniosVector2 neck = [self w2s:p->neck], chest = [self w2s:p->chest];
-        CGPoint lines[] = {CGPointMake(head.x,head.y),CGPointMake(neck.x,neck.y), CGPointMake(neck.x,neck.y),CGPointMake(chest.x,chest.y), CGPointMake(chest.x,chest.y),CGPointMake(pelvis.x,pelvis.y)};
-        CGContextStrokeLineSegments(ctx, lines, 6);
-    }
-    if (self.config.healthBarEnabled) { CGFloat hbX = bx - 8, hp = p->health/100.0f; CGContextSetFillColorWithColor(ctx, [UIColor colorWithWhite:0 alpha:0.5].CGColor); CGContextFillRect(ctx, CGRectMake(hbX, by, 3, bh)); UIColor *hc = hp > 0.6 ? [UIColor greenColor] : hp > 0.3 ? [UIColor yellowColor] : [UIColor redColor]; CGContextSetFillColorWithColor(ctx, hc.CGColor); CGContextFillRect(ctx, CGRectMake(hbX, by+bh*(1-hp), 3, bh*hp)); }
-    if (self.config.nameEnabled) { NSDictionary *a = @{NSFontAttributeName:[UIFont boldSystemFontOfSize:self.config.nameFontSize], NSForegroundColorAttributeName:self.config.nameColor}; [[NSString stringWithUTF8String:p->name] drawAtPoint:CGPointMake(head.x-30, head.y-15) withAttributes:a]; }
-    if (self.config.distanceEnabled) { NSDictionary *a = @{NSFontAttributeName:[UIFont systemFontOfSize:11], NSForegroundColorAttributeName:self.config.distanceColor}; [[NSString stringWithFormat:@"%.0fm", p->distance] drawAtPoint:CGPointMake(head.x-20, pelvis.y+5) withAttributes:a]; }
-}
-
-- (void)renderBomb:(KeniosBomb *)b ctx:(CGContextRef)ctx {
-    KeniosVector2 sp = [self w2s:b->position];
-    if (sp.x < 0) return;
-    UIColor *bc = b->type == 1 ? [UIColor redColor] : b->type == 2 ? [UIColor orangeColor] : [UIColor yellowColor];
-    CGContextSetStrokeColorWithColor(ctx, bc.CGColor); CGContextSetLineWidth(ctx, 2);
-    CGFloat r = b->radius / 10.0f;
-    CGContextAddArc(ctx, sp.x, sp.y, r, 0, 2*M_PI, 0); CGContextStrokePath(ctx);
-    NSString *t = [NSString stringWithFormat:@"%.1fs", b->timeToExplode];
-    [t drawAtPoint:CGPointMake(sp.x-15, sp.y-20) withAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:12], NSForegroundColorAttributeName:[UIColor redColor]}];
-}
-
-- (void)drawFOVCircle:(CGContextRef)ctx {
-    CGContextSetStrokeColorWithColor(ctx, [self.config.fovCircleColor colorWithAlphaComponent:self.config.fovCircleAlpha].CGColor);
-    CGContextSetLineWidth(ctx, 2); CGContextAddArc(ctx, self.sw/2, self.sh/2, self.config.fovCircleRadius, 0, 2*M_PI, 0); CGContextStrokePath(ctx);
-}
-
-- (void)drawCrosshair:(CGContextRef)ctx {
-    CGFloat cx = self.sw/2, cy = self.sh/2, s = self.config.crosshairSize;
-    CGContextSetStrokeColorWithColor(ctx, self.config.crosshairColor.CGColor); CGContextSetLineWidth(ctx, 1);
-    CGContextMoveToPoint(ctx, cx-s, cy); CGContextAddLineToPoint(ctx, cx+s, cy);
-    CGContextMoveToPoint(ctx, cx, cy-s); CGContextAddLineToPoint(ctx, cx, cy+s); CGContextStrokePath(ctx);
-}
-
-- (KeniosVector2)w2s:(KeniosVector3)wp {
-    KeniosVector2 sp = {-1,-1};
-    float w = self.viewMatrix.m[3][0]*wp.x + self.viewMatrix.m[3][1]*wp.y + self.viewMatrix.m[3][2]*wp.z + self.viewMatrix.m[3][3];
-    if (w < 0.01f) return sp;
-    sp.x = (self.viewMatrix.m[0][0]*wp.x + self.viewMatrix.m[0][1]*wp.y + self.viewMatrix.m[0][2]*wp.z + self.viewMatrix.m[0][3]) / w;
-    sp.y = (self.viewMatrix.m[1][0]*wp.x + self.viewMatrix.m[1][1]*wp.y + self.viewMatrix.m[1][2]*wp.z + self.viewMatrix.m[1][3]) / w;
-    sp.x = (sp.x*0.5f+0.5f)*self.sw; sp.y = (1.0f-sp.y*0.5f-0.5f)*self.sh;
-    return sp;
-}
 @end
 
-@interface KeniosESP ()
-@property (nonatomic, strong) KeniosESPOverlay *overlay;
+@interface KeniosFPS ()
+@property (nonatomic, strong) KeniosFPSOverlay *overlay;
+@property (nonatomic, assign) CFTimeInterval lastTime;
+@property (nonatomic, assign) int frameCount;
+@property (nonatomic, assign) int currentFPS;
+@property (nonatomic, assign) int currentPing;
+@property (nonatomic, strong) NSTimer *fpsTimer;
+@property (nonatomic, strong) KeniosPerformanceConfig *config;
 @end
 
-@implementation KeniosESP
+@implementation KeniosFPS
 
 + (instancetype)sharedInstance {
-    static KeniosESP *i = nil; static dispatch_once_t t; dispatch_once(&t, ^{ i = [[KeniosESP alloc] init]; }); return i;
+    static KeniosFPS *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{ instance = [[KeniosFPS alloc] init]; });
+    return instance;
 }
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-        CGFloat w = [UIScreen mainScreen].bounds.size.width, h = [UIScreen mainScreen].bounds.size.height;
-        self.overlay = [[KeniosESPOverlay alloc] initWithFrame:CGRectMake(0,0,w,h)];
-        self.overlay.sw = w; self.overlay.sh = h;
-        dispatch_async(dispatch_get_main_queue(), ^{ [[UIApplication sharedApplication].keyWindow addSubview:self.overlay]; });
+        self.currentFPS = 60;
+        self.currentPing = 0;
+        self.frameCount = 0;
+        self.lastTime = CACurrentMediaTime();
+        self.config = [KeniosConfig sharedInstance].performance;
+        
+        [self setupOverlay];
     }
     return self;
 }
 
-- (void)renderESP {
-    KeniosOffsets *o = [KeniosOffsets sharedInstance];
-    uint64_t lc = [self getLocalController];
-    if (!lc) return;
-    uint64_t cm = *(uint64_t *)(lc + o.PlayerCameraManager);
-    if (!cm) return;
-    self.overlay.viewMatrix = *(KeniosMatrix4x4 *)(cm + o.ViewMatrix);
-    dispatch_async(dispatch_get_main_queue(), ^{ [self.overlay setNeedsDisplay]; });
+- (void)setupOverlay {
+    CGFloat w = [UIScreen mainScreen].bounds.size.width;
+    CGFloat h = [UIScreen mainScreen].bounds.size.height;
+    
+    CGRect frame = CGRectMake(w - 100, 50, 90, 40);
+    self.overlay = [[KeniosFPSOverlay alloc] initWithFrame:frame];
+    self.overlay.hidden = !self.config.fpsDisplay;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindow *window = [UIApplication sharedApplication].keyWindow;
+        if (window) {
+            [window addSubview:self.overlay];
+        }
+    });
+    
+    // Cập nhật mỗi giây
+    self.fpsTimer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                    repeats:YES
+                                                      block:^(NSTimer *timer) {
+        [self calculateFPS];
+    }];
 }
 
-- (uint64_t)getLocalController {
-    KeniosOffsets *o = [KeniosOffsets sharedInstance];
-    uint64_t gi = *(uint64_t *)(g_GWorld + o.OwningGameInstance);
-    if (!gi) return 0;
-    uint64_t lp = *(uint64_t *)(gi + 0x38);
-    return lp ? *(uint64_t *)lp : 0;
+- (void)calculateFPS {
+    CFTimeInterval now = CACurrentMediaTime();
+    CFTimeInterval delta = now - self.lastTime;
+    
+    if (delta >= 1.0) {
+        self.currentFPS = (int)(self.frameCount / delta);
+        self.frameCount = 0;
+        self.lastTime = now;
+        
+        self.overlay.currentFPS = self.currentFPS;
+        self.overlay.currentPing = self.currentPing;
+        [self.overlay updateDisplay];
+    }
 }
 
-- (void)updatePlayers:(NSMutableArray *)p { self.overlay.players = p; }
-- (void)updateBombs:(NSMutableArray *)b { self.overlay.bombs = b; }
-- (void)updateConfig:(KeniosESPConfig *)c { self.overlay.config = c; }
+- (void)incrementFrame {
+    self.frameCount++;
+}
+
+- (void)updateFPS {
+    [self incrementFrame];
+}
+
+- (int)getCurrentFPS {
+    return self.currentFPS;
+}
+
+- (int)getCurrentPing {
+    return self.currentPing;
+}
+
+- (void)setPing:(int)ping {
+    self.currentPing = ping;
+}
+
+- (void)updateConfig:(KeniosPerformanceConfig *)config {
+    self.config = config;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.overlay.hidden = !config.fpsDisplay;
+    });
+}
+
+- (void)setFPSLimit:(int)limit {
+    // Đặt giới hạn FPS (nếu có thể)
+    KENIOS_LOG(@"FPS Limit set to: %d", limit);
+}
+
+- (void)enableFPSBoost {
+    // Tối ưu hiệu năng
+    KENIOS_LOG(@"FPS Boost enabled");
+}
+
 @end
